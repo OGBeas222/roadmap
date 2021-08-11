0ec0dcc2b23320bb2973ab2f5715e23aab4ad734
OGBeas222 <86554211+OGBeas222@users.noreply.github.com>
2021-08-11 18:14:50 +0300

    first commit

---
+++ js/main-2.js
@@ -0,0 +1,8 @@
+var instance = new vidbg(
+    ".roadmap-video", {
+        mp4: "video/roadmap.mp4", // URL or relative path to MP4 video
+        webm: "video/roadmap.webpm", // URL or relative path to webm video
+        poster: "img2/poster_roadmap.jpg", // URL or relative path to fallback image
+        overlay: false, // Boolean to display the overlay or not
+
+    });
